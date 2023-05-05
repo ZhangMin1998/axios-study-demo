a81c54967a50ffbc82c658e4e30454ca31803e31
zhangmin <zhangm@suntang.com>
2023-05-05 17:08:45 +0800

    完成基本功能

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,29 +3,116 @@
     <!-- 联系人列表 -->
     <van-contact-list
       :list="list"
-      default-tag-text="默认"
       @add="onAdd"
       @edit="onEdit"
     />
-
+    <!-- 联系人编辑 -->
+    <van-popup :show="showEdit" position="bottom" :close-on-click-overlay="true" :style="{ height: '100%' }">
+      <van-contact-edit
+      :is-edit="isEdit"
+      :contact-info="editingContact"
+      @save="onSave"
+      @delete="onDelete"
+    />
+    </van-popup>
   </div>
 </template>
 
 <script setup>
 /* eslint-disable */
+import axios from 'axios'
+import { showFailToast, showSuccessToast } from 'vant'
 import { ref, reactive } from 'vue'
 
-// {
-//   id: 1,
-//   name: '',
-//   tel:
-// }
-const list = reactive([])
+const showEdit = ref(false)
+const isEdit = ref(false)
+let list = reactive([])
+let editingContact = reactive({})
+// const instance = null
+const instance = axios.create({
+  baseURL: 'http://localhost:9000/api',
+  timeout: 1000
+})
+
+// 获取列表
+const getList = () => {
+  instance.get('/contactList').then(res => {
+    console.log(res.data.data)
+    list.length = 0
+    list.splice(0, 0, ...res.data.data)
+  }).catch(err => {
+    console.log(err)
+    showFailToast(err)
+  })
+}
+getList()
 
 const onAdd = () => {
-  console.log(666)
+  console.log('添加')
+  showEdit.value = true
+  isEdit.value = false
 }
-const onEdit = () => {
-  console.log(666)
+const onEdit = (info) => {
+  showEdit.value = true
+  isEdit.value = true
+  editingContact = info
+}
+const onSave = (info) => {
+  console.log('保存')
+  if (isEdit.value) {
+    // 编辑保存
+    console.log('编辑保存')
+    instance.put('/contact/edit', info).then(res => {
+      if (res.data.code === 200) {
+        showSuccessToast('编辑成功')
+        showEdit.value = false
+      }
+    }).catch(err => {
+      console.log(err)
+      showFailToast(err)
+    }).finally(() => {
+      getList()
+    })
+  } else {
+    // 新建保存
+    instance.post('/contact/new/json', info).then(res => {
+      if (res.data.code === 200) {
+        showSuccessToast('保存成功')
+        showEdit.value = false
+      }
+    }).catch(err => {
+      console.log(err)
+      showFailToast(err)
+    }).finally(() => {
+      getList()
+    })
+  }
+}
+const onDelete = (info) => {
+  console.log('删除')
+  instance.delete('/contact', {
+    params: {
+      id: info.id
+    }
+  }).then(res => {
+    if (res.data.code === 200) {
+      showSuccessToast('删除成功')
+      showEdit.value = false
+    }
+  }).catch(err => {
+    console.log(err)
+    showFailToast(err)
+  }).finally(() => {
+    getList()
+  })
 }
 </script>
+
+<style lang="less" scoped>
+// .van-concat-list__add{
+//   z-index: 0;
+// }
+// .van-popup{
+//   height: 100% !important;
+// }
+</style>
